9172ffe79ac366762bcf9ad5e6716135518394e2
Joe <joedolina@gmail.com>
2024-01-26 07:48:38 -0500

    Add i2c to DT, new src; doesn't compile

---
+++ imu/src/main.c
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2016 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/i2c.h>
+
+/* 1000 msec = 1 sec */
+#define SLEEP_TIME_MS   5000
+
+/* The devicetree node identifier for the "led0" alias. */
+#define LED0_NODE DT_ALIAS(led0)
+
+/*
+ * A build error on this line means your board is unsupported.
+ * See the sample documentation for information on how to fix this.
+ */
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+static const struct i2c_dt_spec i2c_bus = I2C_DT_SPEC_GET(&i2c4);
+
+int main(void)
+{
+	int ret;
+
+	if (!gpio_is_ready_dt(&led)) {
+		return 0;
+	}
+
+	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+	if (ret < 0) {
+		return 0;
+	}
+
+
+
+	while (1) {
+		ret = gpio_pin_toggle_dt(&led);
+		if (ret < 0) {
+			return 0;
+		}
+		k_msleep(SLEEP_TIME_MS);
+	}
+	return 0;
+}

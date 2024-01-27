39fe7b925c4b9a1a14c61d426606f8530bb706b4
Joe <joedolina@gmail.com>
2024-01-27 17:00:31 -0500

    Added device.h, can now label i2c4 node

--- imu/src/main.c
+++ imu/src/main.c
@@ -7,19 +7,21 @@
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/i2c.h>
+#include <zephyr/device.h>
 
 /* 1000 msec = 1 sec */
 #define SLEEP_TIME_MS   5000
 
 /* The devicetree node identifier for the "led0" alias. */
 #define LED0_NODE DT_ALIAS(led0)
+#define I2C4 DT_NODELABEL(i2c4)
 
 /*
  * A build error on this line means your board is unsupported.
  * See the sample documentation for information on how to fix this.
  */
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
-static const struct i2c_dt_spec i2c_bus = I2C_DT_SPEC_GET(i2c4);
+//static const struct i2c_dt_spec i2c_bus = I2C_DT_SPEC_GET(I2C4);
 
 int main(void)
 {

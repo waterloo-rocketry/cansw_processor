4b25b70ae841f3d029e1a3252aa31e70aab8e368
Joe <joedolina@gmail.com>
2024-01-27 10:57:55 -0500

    Add ADC to dts, get dts spec in main

--- hello_world/src/main.c
+++ hello_world/src/main.c
@@ -6,8 +6,12 @@
 
 #include <stdio.h>
 #include <zephyr/logging/log.h>
+#include <zephyr/drivers/adc.h>
 
 LOG_MODULE_REGISTER(a, LOG_LEVEL_DBG);
+
+static const struct adc_channel_cfg ch0_cfg_dt =
+    ADC_CHANNEL_CFG_DT(DT_CHILD(DT_NODELABEL(adc1), channel_0));
 
 int main(void)
 {

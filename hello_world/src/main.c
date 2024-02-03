c40c644b19de04badb5fe23d7db521630d5b54b1
Joe <joedolina@gmail.com>
2024-02-03 16:03:58 -0500

    dts builds, dt_get works

--- hello_world/src/main.c
+++ hello_world/src/main.c
@@ -6,13 +6,18 @@
 
 #include <stdio.h>
 #include <zephyr/logging/log.h>
+#include <zephyr/drivers/can.h>
+#include <zephyr/device.h>
 
 LOG_MODULE_REGISTER(a, LOG_LEVEL_DBG);
+#define FDCAN1 DT_NODELABEL(fdcan1)
+static const struct device *const can_dev = DEVICE_DT_GET(FDCAN1);
 
 int main(void)
 {
 	
 	LOG_INF("Hello World! \n");
 	//printf("Hello World!\n");
+
 	return 0;
 }

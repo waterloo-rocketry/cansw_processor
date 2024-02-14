2fc0a6a03a260fa4a69b6eba189c21a55b2bde3b
Joshua Dietrich <59072117+Taggagii@users.noreply.github.com>
2024-02-14 18:57:20 -0500

    fixing up test, fully functional and accurate now

--- zbus/src/main.c
+++ zbus/src/main.c
@@ -19,12 +19,18 @@ struct our_zbus_message {
 static bool our_channel_validator(const void *message, size_t message_size) {
 	ARG_UNUSED(message_size);
 
-	const struct out_zbus_message *recieved_message = message;
+	const struct our_zbus_message *recieved_message = message;
 
-	// bool validType = (recieved_message->type >= 0 && recieved_message->type <= 5);
-	bool validType = 1;
-	// bool contentSizeCorrect = (recieved_message->contentSize == strlen(recieved_message->content));
-	bool contentSizeCorrect = 1;
+	bool validType = (recieved_message->type >= 0 && recieved_message->type <= 5);
+	bool contentSizeCorrect = (recieved_message->contentSize == strlen(recieved_message->content));
+
+	if (!validType) {
+		LOG_INF("Invalid type %d", recieved_message->type);
+	}
+
+	if (!contentSizeCorrect) {
+		LOG_INF("Content size mismatched, expected %d found %d", recieved_message->contentSize, strlen(recieved_message->content));
+	}
 
 	return (validType && contentSizeCorrect);
 }
@@ -39,7 +45,7 @@ ZBUS_CHAN_DEFINE(
 );
 
 static void observer_function(const struct zbus_channel *chan) {
-	if (chan != &our_channel) return; // todo: verify that this is necessary (and not broken)
+	if (chan != &our_channel) return;
 
 	const struct our_zbus_message *recieved_message = zbus_chan_const_msg(chan);
 
@@ -83,7 +89,7 @@ K_THREAD_DEFINE(
 int main(void) {
 	LOG_INF("======= Starting simple zbus sample =======");
 
-	LOG_INF("Publishing a message to our channel...");
+	LOG_INF("Publishing a valid message to channel...");
 	struct our_zbus_message message = {
 		.type = 0,
 		.content = "testing",
@@ -91,20 +97,20 @@ int main(void) {
 	};
 	int error = zbus_chan_pub(&our_channel, &message, K_SECONDS(1));
 
-	if (error == 0) {
+	if (error) {
 		LOG_INF("We failed to send a valid message, this is bad!");
 	}
 
 	k_msleep(1000);
 
-	LOG_INF("Publishing a new message to our channel...");
+	LOG_INF("Publishing invalid message to channel...");
 	message.content = "This is a new message";
 	int error2 = zbus_chan_pub(&our_channel, &message, K_SECONDS(1));
 
-	if (error2 == 0 && error == 0) {
+	if (error2 && !error) {
 		LOG_INF("Invalid message caught by the validator!");
-	} else if (error2 == 0) {
-		LOG_INF("Something other than the validator went wrong here!");
+	} else if (error2) {
+		LOG_INF("Both messages failed! This is bad");
 	}
 
 	LOG_INF("Exiting");

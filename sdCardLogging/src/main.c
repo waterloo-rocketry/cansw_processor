aaa464562542e0ba3f049640e410c90b6c7953b9
Joshua Dietrich <59072117+Taggagii@users.noreply.github.com>
2024-02-14 19:23:34 -0500

    logging types and general stucture

--- sdCardLogging/src/main.c
+++ sdCardLogging/src/main.c
@@ -10,51 +10,81 @@
 #include <zephyr/zbus/zbus.h>
 LOG_MODULE_DECLARE(zbus, CONFIG_ZBUS_LOG_LEVEL);
 
-struct our_zbus_message {
-	int type;
-	char* content;
+enum LoggingTypes {
+	WRITE,
+	READ,
+	NUMBER_OF_LOGGING_TYPES,
 };
 
-static bool our_channel_validator(const void *message, size_t message_size) {
+typedef struct LoggingMessage {
+	enum LoggingTypes type;
+	char* content;
+} LoggingMessage;
+
+static bool log_channel_validator(const void *message, size_t message_size) {
 	ARG_UNUSED(message_size);
 
-	const struct our_zbus_message *recieved_message = message;
+	const LoggingMessage *recieved_message = message;
 
-	bool validType = (recieved_message->type >= 0 && recieved_message->type <= 5);
+	bool validType = (recieved_message->type >= 0 && recieved_message->type < NUMBER_OF_LOGGING_TYPES);
+
+	if (!validType) {
+		LOG_INF("Invalid type %d", recieved_message->type);
+	}
 
 	return validType;
 }
 
 ZBUS_CHAN_DEFINE(
-	our_channel, // name of channel
-	struct our_zbus_message, // type of message on this channel
-	our_channel_validator, // validator (function to flush out invalid messages)
+	LoggingChannel, // name of channel
+	LoggingMessage, // type of message on this channel
+	log_channel_validator, // validator (function to flush out invalid messages)
 	NULL, // User Data
-	ZBUS_OBSERVERS(our_channel_listener, our_channel_subscriber), // observers
-	ZBUS_MSG_INIT(.type = 0, .content = "", .contentSize = 0) // inital value
+	ZBUS_OBSERVERS(loggingListener), // observers
+	ZBUS_MSG_INIT(.type = 0, .content = "") // inital value
 );
 
-static void observer_function(const struct zbus_channel *chan) {
-	if (chan != &our_channel) return;
+static void loggingListenerFunction(const struct zbus_channel *chan) {
+	if (chan != &LoggingChannel) return;
 
-	const struct our_zbus_message *recieved_message = zbus_chan_const_msg(chan);
+	const LoggingMessage *message = zbus_chan_const_msg(chan);
 
 	LOG_INF(
-		"Observer got a message of type %d, size %d, with content '%s'",
-		recieved_message->type,
-		recieved_message->contentSize,
-		recieved_message->content
+		"Listener got a message of type %d, with content '%s'",
+		message->type,
+		message->content
 	);
 }
 
-ZBUS_LISTENER_DEFINE(our_channel_listener, observer_function);
+ZBUS_LISTENER_DEFINE(loggingListener, loggingListenerFunction);
 
 
 int main(void) {
-	LOG_INF("======= Starting simple SD Card Logging Test =======");
+	LOG_INF("======= Starting simple sd card logging sample =======");
 
+	LOG_INF("Publishing a valid message to channel...");
+	LoggingMessage message = {
+		.type = WRITE,
+		.content = "testing"
+	};
+	int error = zbus_chan_pub(&LoggingChannel, &message, K_SECONDS(1));
 
+	if (error) {
+		LOG_INF("We failed to send a valid message, this is bad!");
+	}
 
+	k_msleep(1000);
+
+	LOG_INF("Publishing invalid message to channel...");
+	message.content = "This is a new message";
+	message.type = NUMBER_OF_LOGGING_TYPES;
+	int error2 = zbus_chan_pub(&LoggingChannel, &message, K_SECONDS(1));
+
+	if (error2 && !error) {
+		LOG_INF("Invalid message caught by the validator!");
+	} else if (error2) {
+		LOG_INF("Both messages failed! This is bad");
+	}
 
 	LOG_INF("Exiting");
 	return 0;
